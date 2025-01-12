29123f0b6befa8d5a36ec95835dda01c48b24998
ali-raza-166 <90212743+ali-raza-166@users.noreply.github.com>
2025-01-13 02:20:16 +0500

    Revert "feat: Integrate OpenAI API, Implement Embedding Service and Cosine Similarity Class"

--- SemanticSimilarityAnalysis.Proj/Model/Embedding.cs
+++
@@ -1,9 +0,0 @@
-namespace SemanticSimilarityAnalysis.Proj.Model
-{
-    public class Embedding(int index, string text, List<float> embeddingVector)
-    {
-        public int Index { get; set; } = index;
-        public string Text { get; set; } = text;
-        public List<float> EmbeddingVector { get; set; } = embeddingVector;
-    }
-}
