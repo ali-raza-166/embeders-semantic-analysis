9450719bfbce6b11dc89fe60d3e67ce0a263f08e
haris <mhmisb@yahoo.com>
2025-01-20 12:38:59 +0100

    refactor: EuclideanDistance_MSTEST_TestMethod_SimilarLength

--- SemanticSimilarityAnalysis.Test/EuclideanDistanceTests.cs
+++ SemanticSimilarityAnalysis.Test/EuclideanDistanceTests.cs
@@ -7,6 +7,22 @@ namespace SemanticSimilarityAnalysis.Tests
     [TestClass]
     public class EuclideanDistanceTests
     {
-        
+        [TestMethod]
+        public void ComputeEuclideanDistance_ValidInputs_ReturnsCorrectResult()
+        {
+            
+            var euclideanDistance = new EuclideanDistance();
+            var vectorA = new List<float> { 1, 2, 3 };
+            var vectorB = new List<float> { 4, 5, 6 };
+
+            
+            var expected = 5.196152422;
+
+            
+            var result = euclideanDistance.ComputeEuclideanDistance(vectorA, vectorB);
+
+            
+            Assert.AreEqual(expected, result, 0.000001, "Euclidean distance calculation is incorrect.");
+        }
     }
 }
