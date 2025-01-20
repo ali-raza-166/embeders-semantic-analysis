4a06feeab2c561e8ec658a1df113fc8caa5a7cef
haris <mhmisb@yahoo.com>
2025-01-20 12:20:49 +0100

    test: EuclideanDistance_MSTEST_TestClass

---
+++ SemanticSimilarityAnalysis.Test/EuclideanDistanceTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SemanticSimilarityAnalysis.Proj.Utils; 
+using System.Collections.Generic;
+
+namespace SemanticSimilarityAnalysis.Tests
+{
+    [TestClass]
+    public class EuclideanDistanceTests
+    {
+        
+    }
+}
