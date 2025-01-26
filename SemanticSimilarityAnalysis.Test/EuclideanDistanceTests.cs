ef55ecde4670b6a6999cf3d7a80d80ef56aee3b6
haris <mhmisb@yahoo.com>
2025-01-26 14:58:04 +0100

    style: Comments added to the EuclideanDistanceTests

--- SemanticSimilarityAnalysis.Test/EuclideanDistanceTests.cs
+++ SemanticSimilarityAnalysis.Test/EuclideanDistanceTests.cs
@@ -1,27 +1,27 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SemanticSimilarityAnalysis.Proj.Utils; 
+using SemanticSimilarityAnalysis.Proj.Utils; // Reference to the main project
 using System.Collections.Generic;
 
 namespace SemanticSimilarityAnalysis.Tests
 {
-    [TestClass]
+    [TestClass] // Marks this class as containing unit tests
     public class EuclideanDistanceTests
     {
-        [TestMethod]
+        [TestMethod] // Marks this method as a unit test
         public void ComputeEuclideanDistance_ValidInputs_ReturnsCorrectResult()
         {
-            
+            // Arrange
             var euclideanDistance = new EuclideanDistance();
             var vectorA = new List<float> { 1, 2, 3 };
             var vectorB = new List<float> { 4, 5, 6 };
 
-            
+            // Expected result manually calculated
             var expected = 5.196152422;
 
-            
+            // Act
             var result = euclideanDistance.ComputeEuclideanDistance(vectorA, vectorB);
 
-            
+            // Assert
             Assert.AreEqual(expected, result, 0.000001, "Euclidean distance calculation is incorrect.");
         }
 
@@ -29,12 +29,12 @@ namespace SemanticSimilarityAnalysis.Tests
         [ExpectedException(typeof(System.ArgumentException))]
         public void ComputeEuclideanDistance_DifferentLengths_ThrowsException()
         {
-            
+            // Arrange
             var euclideanDistance = new EuclideanDistance();
             var vectorA = new List<float> { 1, 2, 3 };
             var vectorB = new List<float> { 4, 5 };
 
-            
+            // Act
             euclideanDistance.ComputeEuclideanDistance(vectorA, vectorB);
         }
 
