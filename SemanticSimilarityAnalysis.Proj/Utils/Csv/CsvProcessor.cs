7938bdc1949ca6b3cf8bc14e3e1f60db988c7a45
haris <mhmisb@yahoo.com>
2025-02-01 12:46:02 +0100

    feat: Generate_Embeddings_Injecting OpenAiEmbeddingService into CsvProcessor

--- SemanticSimilarityAnalysis.Proj/Utils/Csv/CsvProcessor.cs
+++ SemanticSimilarityAnalysis.Proj/Utils/Csv/CsvProcessor.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using SemanticSimilarityAnalysis.Proj.Services;
 
 namespace SemanticSimilarityAnalysis.Proj.Utils
 {
     public class CsvProcessor
     {
+        private readonly OpenAiEmbeddingService _embeddingService;
         public CsvProcessor()
         {
+            _embeddingService = embeddingService ?? throw new ArgumentNullException(nameof(embeddingService));
         }
     }
 }
