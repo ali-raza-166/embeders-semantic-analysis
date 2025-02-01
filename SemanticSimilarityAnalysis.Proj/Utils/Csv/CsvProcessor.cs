5feb9d4516fd099210b0f8029ef4eb4fbcfdf838
haris <mhmisb@yahoo.com>
2025-02-01 12:38:06 +0100

    feat: Generate_Embeddings_Csv Processor Class structure

---
+++ SemanticSimilarityAnalysis.Proj/Utils/Csv/CsvProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SemanticSimilarityAnalysis.Proj.Utils
+{
+    public class CsvProcessor
+    {
+        public CsvProcessor()
+        {
+        }
+    }
+}
