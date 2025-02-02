fccb8d5489d0d25d1cc79e53c9fc1035e1358147
Jack <khonhong46632@gmail.com>
2025-02-02 09:08:06 +0100

    feat: add movie's embeddings into json file

---
+++ SemanticSimilarityAnalysis.Proj/Utils/Csv/CsvProcessor.cs
@@ -0,0 +1,18 @@
+﻿using SemanticSimilarityAnalysis.Proj.Models;
+using System.Text.Json;
+
+namespace SemanticSimilarityAnalysis.Proj.Utils.Csv
+{
+    internal class CsvProcessor
+    {
+        public class CsvSaver
+        {
+            public static async Task SaveEmbeddingsToJsonAsync(List<MovieModel> movies, string jsonFilePath)
+            {
+                var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+                var jsonData = JsonSerializer.Serialize(movies, jsonOptions);
+                await File.WriteAllTextAsync(jsonFilePath, jsonData);
+            }
+        }
+    }
+}
