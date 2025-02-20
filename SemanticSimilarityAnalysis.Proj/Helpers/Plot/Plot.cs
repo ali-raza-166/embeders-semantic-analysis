54eec997abe10e2d18a3169f262883584945ca4a
haris <mhmisb@yahoo.com>
2025-02-21 00:35:36 +0100

    feat: add RecordSimilarityData class to store similarity results

---
+++ SemanticSimilarityAnalysis.Proj/Helpers/Plot/Plot.cs
@@ -0,0 +1,10 @@
+﻿public class RecordSimilarityData
+{
+    public string RecordId { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+    public float Similarity1 { get; set; }
+    public float Similarity2 { get; set; }
+    public float Similarity3 { get; set; }
+}
