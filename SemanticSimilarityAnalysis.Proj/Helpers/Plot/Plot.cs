f4e969f52d14248b3e02ab7a58c5ad746656065a
haris <mhmisb@yahoo.com>
2025-02-21 00:44:31 +0100

    feat: add function to compute cosine similarity between vectors

--- SemanticSimilarityAnalysis.Proj/Helpers/Plot/Plot.cs
+++ SemanticSimilarityAnalysis.Proj/Helpers/Plot/Plot.cs
@@ -16,3 +16,19 @@ public async Task<List<RecordEmbedding>> loadRecordEmbeddingsFromFile(string fil
 
     return records.Where(r => r.Attribute == attribute).ToList();
 }
+public float computeCosineSimilarity(float[] vectorA, float[] vectorB)
+{
+    float dotProduct = 0, magnitudeA = 0, magnitudeB = 0;
+
+    for (int i = 0; i < vectorA.Length; i++)
+    {
+        dotProduct += vectorA[i] * vectorB[i];
+        magnitudeA += vectorA[i] * vectorA[i];
+        magnitudeB += vectorB[i] * vectorB[i];
+    }
+
+    magnitudeA = (float)Math.Sqrt(magnitudeA);
+    magnitudeB = (float)Math.Sqrt(magnitudeB);
+
+    return magnitudeA == 0 || magnitudeB == 0 ? 0 : dotProduct / (magnitudeA * magnitudeB);
+}
