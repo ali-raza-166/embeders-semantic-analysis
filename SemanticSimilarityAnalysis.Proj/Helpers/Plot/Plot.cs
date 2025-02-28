d54722265b3fe2fe0201f1244ece7377da52663b
haris <mhmisb@yahoo.com>
2025-02-28 23:20:49 +0100

    feat: mapping similarity values to coordinate system

--- SemanticSimilarityAnalysis.Proj/Helpers/Plot/Plot.cs
+++ SemanticSimilarityAnalysis.Proj/Helpers/Plot/Plot.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace SemanticSimilarityAnalysis.Proj.Models
+{
+    public class SimilarityPlotPoint
+    {
+        public string Label { get; set; }
+        public double SimilarityWithInput1 { get; set; }
+        public double SimilarityWithInput2 { get; set; }
+        public double SimilarityWithInput3 { get; set; }
+
+        public double X => SimilarityWithInput1;
+        public double Y => SimilarityWithInput2;
+        public double Z => SimilarityWithInput3;
+    }
+}
