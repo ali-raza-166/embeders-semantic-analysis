cd883972e9c4f627d2181a4e4f40e854a2d2c5f4
Ali Raza <>
2025-03-02 07:15:55 +0100

    feat: implement multilingual support for vector search and retrieval

--- SemanticSimilarityAnalysis.Proj/Services/OpenAiTextGenerationService.cs
+++ SemanticSimilarityAnalysis.Proj/Services/OpenAiTextGenerationService.cs
@@ -15,9 +15,10 @@ namespace SemanticSimilarityAnalysis.Proj.Services
         public async Task<string> GenerateTextAsync( string query, List<string> paragraphs)
         {
             var contextText = string.Join("\n\n", paragraphs);
+            Console.WriteLine($"Context: {contextText}");
 
             var prompt =
-                $"Here are paragraphs:\n\n{contextText}\n\nBased on the above information, answer the following question:\n{query}";
+                $"I will give you text paragraphs and a question. You must answer the question in the light of the paragraph I provided as context. The paragrpahs can be in different languages. Always answer back in the language in which the question is asked. Here are paragraphs:\n\n{contextText}\n\nBased on the above information, answer the following question:\n{query} \n Keep in mind, always use the same language of answer as the language of query. You mind recieve paragraphs in different language but use them to create the answer";
 
             ChatCompletion completion = await _openAiClient.CompleteChatAsync(prompt);
             return completion.Content[0].Text;
