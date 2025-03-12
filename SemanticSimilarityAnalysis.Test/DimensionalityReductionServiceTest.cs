b2a04f2a6903d2a439e43b0bf2820f3eda103162
haris <mhmisb@yahoo.com>
2025-03-13 00:33:07 +0100

    feat: adding unit test for TSNE dimensionality reduction

---
+++ SemanticSimilarityAnalysis.Test/DimensionalityReductionServiceTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SemanticSimilarityAnalysis.Proj.Services;
+using System.Collections.Generic;
+
+namespace SemanticSimilarityAnalysis.Tests
+{
+    [TestClass]
+    public class DimensionalityReductionServiceTests
+    {
+        [TestMethod]
+        public void ReduceDimensionsUsingTsne_ShouldReduceToTargetDimensions()
+        {
+            // Arrange
+            var service = new DimensionalityReductionService(2);
+            var inputData = new List<List<float>>
+            {
+                new List<float> { 1.0f, 2.0f, 3.0f },
+                new List<float> { 4.0f, 5.0f, 6.0f },
+                new List<float> { 7.0f, 8.0f, 9.0f }
+            };
+
+            // Act
+            var result = service.ReduceDimensionsUsingTsne(inputData, 2);
+
+            // Assert
+            Assert.IsNotNull(result, "Result should not be null.");
+            Assert.AreEqual(2, result.ColumnCount, "Result should have exactly 2 columns.");
+        }
+    }
+}
