5c2e0bd20ee9290cc6eb4eadf9ddb95db927626a
haris <mhmisb@yahoo.com>
2025-03-13 02:22:50 +0100

    refactor+feat: code refactoring and adding unit test for invalid dimension handling in TSNE

--- SemanticSimilarityAnalysis.Test/DimensionalityReductionServiceTest.cs
+++ SemanticSimilarityAnalysis.Test/DimensionalityReductionServiceTest.cs
@@ -1,13 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SemanticSimilarityAnalysis.Proj.Services;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System;
 using System.Collections.Generic;
+
+
 
 namespace SemanticSimilarityAnalysis.Tests
 {
     [TestClass]
     public class DimensionalityReductionServiceTests
     {
-        // Test TSNE dimensionality reduction
         [TestMethod]
         public void ReduceDimensionsUsingTsne_ShouldReduceToTargetDimensions()
         {
@@ -27,5 +31,27 @@ namespace SemanticSimilarityAnalysis.Tests
             Assert.IsNotNull(result, "Result should not be null.");
             Assert.AreEqual(2, result.ColumnCount, "Result should have exactly 2 columns.");
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReduceDimensionsUsingTsne_ShouldThrowExceptionForInvalidDimensions()
+        {
+            
+            var service = new DimensionalityReductionService(2);
+            var inputData = new List<List<float>>
+            {
+                new List<float> { 1.0f, 2.0f, 3.0f },
+                new List<float> { 4.0f, 5.0f, 6.0f },
+                new List<float> { 7.0f, 8.0f, 9.0f }
+             };
+
+           
+            service.ReduceDimensionsUsingTsne(inputData, 1); 
+        }
+
+        
+
+
     }
 }
+
