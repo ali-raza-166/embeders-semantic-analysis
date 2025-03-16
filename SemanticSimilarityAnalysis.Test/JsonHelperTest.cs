3f5fadecb6d4a0fc8def4f8199de42e2c9708c01
haris <mhmisb@yahoo.com>
2025-03-16 02:04:40 +0100

    feat: adding test method 1

--- SemanticSimilarityAnalysis.Test/JsonHelperTest.cs
+++ SemanticSimilarityAnalysis.Test/JsonHelperTest.cs
@@ -6,8 +6,23 @@ namespace SemanticSimilarityAnalysis.Proj.Helpers.Json
     [TestClass]
     public class JsonHelper
     {
-       
+        [TestMethod]
+        public List<MultiEmbeddingRecord> GetRecordFromJson(string filePath)
+        {
+            try
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    return JsonSerializer.Deserialize<List<MultiEmbeddingRecord>>(stream)
+                        ?? throw new InvalidOperationException("Failed to load embeddings.");
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new InvalidOperationException("File not found.", ex);
+            }
 
+        }
     }
 
 }
