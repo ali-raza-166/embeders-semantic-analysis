8f391394cf339cab83874a911a42fc35e90ebe73
haris <mhmisb@yahoo.com>
2025-03-16 02:01:03 +0100

    feat: adding json file and initializing jsonhelper test class

---
+++ SemanticSimilarityAnalysis.Test/JsonHelperTest.cs
@@ -0,0 +1,13 @@
+using SemanticSimilarityAnalysis.Proj.Models;
+using System.Text.Json;
+
+namespace SemanticSimilarityAnalysis.Proj.Helpers.Json
+{
+    [TestClass]
+    public class JsonHelper
+    {
+       
+
+    }
+
+}
