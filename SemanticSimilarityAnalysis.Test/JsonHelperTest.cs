dede7e84866f81585f5443dbeb57c9c16a86b732
haris <mhmisb@yahoo.com>
2025-03-16 02:08:51 +0100

    feat + docs: adding test method 2 and commenting the code

--- SemanticSimilarityAnalysis.Test/JsonHelperTest.cs
+++ SemanticSimilarityAnalysis.Test/JsonHelperTest.cs
@@ -6,6 +6,7 @@ namespace SemanticSimilarityAnalysis.Proj.Helpers.Json
     [TestClass]
     public class JsonHelper
     {
+        // Method to read and deserialize JSON from a file 
         [TestMethod]
         public List<MultiEmbeddingRecord> GetRecordFromJson(string filePath)
         {
@@ -21,6 +22,21 @@ namespace SemanticSimilarityAnalysis.Proj.Helpers.Json
             {
                 throw new InvalidOperationException("File not found.", ex);
             }
+
+        }
+        // Ensure the directory exists and save the JSON
+        [TestMethod]
+        public async Task SaveRecordToJson(List<MultiEmbeddingRecord> records, string jsonFilePath)
+        {
+            var outputDirectory = Path.GetDirectoryName(jsonFilePath);
+
+            if (!string.IsNullOrWhiteSpace(outputDirectory) && !Directory.Exists(outputDirectory))
+            {
+                Directory.CreateDirectory(outputDirectory);
+            }
+
+            var jsonData = JsonSerializer.Serialize(records, new JsonSerializerOptions { WriteIndented = true });
+            await File.WriteAllTextAsync(jsonFilePath, jsonData);
 
         }
     }
